ae5038c0e63a25538235668f6ba7cc418d41b338
Takeshi Takatsudo <takazudo@gmail.com>
2021-06-09 12:34:41 +0900

    added p

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
+    <p>The quick brown fox jumps over the lazy dog. </p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
